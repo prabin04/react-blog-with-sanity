e48339a87117efb94f26062f98794b8df019b044
Prabin Parajuli <prabin.b9@gmail.com>
2020-12-08 19:16:24 +0100

    Setting up React Routing for your blog

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {BrowserRouter, Router} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+        <Route component={AllPosts} path="/path" exact />
+        <Route component={OnePost} path="/:slug" exact />
+    </BrowserRouter>
   );
 }
 
---
+++ src/client.js
@@ -0,0 +1,6 @@
+import sanityClinet from '@sanity/client';
+
+export default: sanityClinet{
+    projectId: "zyvfjpri",
+    dataset: "proeduction"
+}
